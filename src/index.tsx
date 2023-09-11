51b71257f0cb8dcc0501231b73974a8f97a0769d
Tyler Schloesser <tylerschloesser@gmail.com>
2023-09-11 16:52:59 -0700

    add context

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,34 @@
+import React, { useContext, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 
+interface User {
+  name: string
+}
+
+interface AppContext {
+  users: User[]
+}
+
+const AppContext = React.createContext<AppContext>(null!)
+
 function Home() {
+  const context = useContext(AppContext)
+  return <pre>{JSON.stringify(context)}</pre>
+}
+
+function App() {
+  const [context] = useState<AppContext>({
+    users: [{ name: 'Player 1' }, { name: 'Player 2' }],
+  })
+
   return (
-    <>
+    <AppContext.Provider value={context}>
       <h1>Word Game</h1>
-    </>
+      <Home />
+    </AppContext.Provider>
   )
 }
 
 const root = createRoot(document.getElementById('root')!)
-root.render(<Home />)
+root.render(<App />)
